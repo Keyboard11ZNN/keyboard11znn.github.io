d6d98f749681ac8508ce5506f2df094fe3ffc2ed
stan423321 <8921047+stan423321@users.noreply.github.com>
2020-01-10 17:19:57 -0500

    added some customization

--- source/conf.py
+++ source/conf.py
@@ -52,9 +52,13 @@ highlight_language = 'cpp'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# here's my super turbo fancy minimal logo which totally didn't take forever
+# to make - stan
+html_logo = '_static/keyboard11banner.svg'
