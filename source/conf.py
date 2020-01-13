3b57a7a80ec0f7ba5288a0dd46b0cb74255374b8
stan423321 <8921047+stan423321@users.noreply.github.com>
2020-01-13 12:50:04 -0500

    added minimum compat with Github hosting

--- source/conf.py
+++ source/conf.py
@@ -64,7 +64,7 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_extra_path = ['_extra']
 # here's my super turbo fancy minimal logo which totally didn't take forever
 # to make - stan
 html_logo = '_static/keyboard11banner.svg'
